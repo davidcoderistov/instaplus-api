42b4102331ea44ad00609f60c57263debe98b57b
davidcoderistov <davidcoderistov@gmail.com>
2023-06-27 22:48:56 +0200

    Initialize inversify express app

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,27 @@
-import express from 'express'
+import 'reflect-metadata'
+import { InversifyExpressServer } from 'inversify-express-utils'
+import container from './container/container'
+import { TYPES } from './container/types'
+import { IDatabaseService } from './modules/database/IDatabase.service'
+import { json, urlencoded } from 'express'
+import cors from 'cors'
 import morgan from 'morgan'
 
-const app = express()
-app.use(morgan('dev'))
 
-const port = Number(process.env.PORT || 8080)
-app.listen(port, () => {
-    console.log(`Server listening on port ${port}`)
-})
+(async () => {
+    const server = new InversifyExpressServer(container)
+    await container.get<IDatabaseService>(TYPES.IDatabaseService).initialize()
+
+    const port = Number(process.env.PORT || 8080)
+    server
+        .setConfig(app => {
+            app.use(morgan('dev'))
+            app.use(cors())
+            app.use(json())
+            app.use(urlencoded({ extended: true }))
+        })
+        .build()
+        .listen(port, () => {
+            console.log(`Server listening on port ${port}`)
+        })
+})()
