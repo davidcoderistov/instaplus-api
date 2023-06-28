62bc911189e9ca3b9fd039812710fe0bbbfa2ce8
davidcoderistov <davidcoderistov@gmail.com>
2023-06-28 15:31:37 +0200

    Add shared types

---
+++ src/shared/types/index.ts
@@ -0,0 +1,9 @@
+export interface Context {
+    setRefreshTokenCookie: (refreshToken: string, immediate?: boolean) => void
+    getRefreshTokenCookie: () => string | undefined
+    userId: string | null
+}
+
+export interface WsContext {
+    userId: string
+}
