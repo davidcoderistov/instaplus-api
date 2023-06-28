b7a9606f33858b2bd73db8122bcf779e64f93d76
davidcoderistov <davidcoderistov@gmail.com>
2023-06-28 22:03:37 +0200

    Expose express app and add body parses middleware

--- src/core/http.server/IHttpServer.service.ts
+++ src/core/http.server/IHttpServer.service.ts
@@ -1,8 +1,11 @@
 import { Server } from 'http'
+import { Application } from 'express'
 
 
 export interface IHttpServerService {
     initialize(): void
 
     getHttpServer(): Server
+
+    getExpressApp(): Application
 }
--- src/core/http.server/http.server.service.ts
+++ src/core/http.server/http.server.service.ts
@@ -1,11 +1,12 @@
 import http, { Server } from 'http'
+import { Application } from 'express'
 import { injectable } from 'inversify'
 import { InversifyExpressServer } from 'inversify-express-utils'
 import container from '../../container/container'
 import { IHttpServerService } from './IHttpServer.service'
 import morgan from 'morgan'
 import cors from 'cors'
-import { json, urlencoded } from 'express'
+import { json } from 'body-parser'
 
 
 @injectable()
@@ -13,18 +14,26 @@ export class HttpServerService implements IHttpServerService {
 
     private httpServer!: Server
 
+    private expressApp!: Application
+
     public initialize() {
         const server = new InversifyExpressServer(container)
 
         this.httpServer = http.createServer(server.setConfig(app => {
-            app.use(morgan('dev'))
-            app.use(cors())
-            app.use(json())
-            app.use(urlencoded({ extended: true }))
+            app.use('/api',
+                morgan('dev'),
+                cors({ origin: 'http://localhost:3000', credentials: true }),
+                json(),
+            )
+            this.expressApp = app
         }).build())
     }
 
     public getHttpServer(): Server {
         return this.httpServer
+    }
+
+    public getExpressApp(): Application {
+        return this.expressApp
     }
 }
