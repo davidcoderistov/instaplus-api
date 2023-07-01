2d1ee8126c21eaebc43827e5349bda1c43c0bdf9
davidcoderistov <davidcoderistov@gmail.com>
2023-07-01 16:59:34 +0200

    Register chat service and chat resolver

--- src/container/inversify.config.ts
+++ src/container/inversify.config.ts
@@ -8,8 +8,13 @@ import { IApolloServerService } from '../core/apollo.server/IApolloServer.servic
 import { ApolloServerService } from '../core/apollo.server/apollo.server.service'
 import { IGraphQLSchemaService } from '../core/graphql.schema/IGraphQLSchema.service'
 import { GraphQLSchemaService } from '../core/graphql.schema/graphql.schema.service'
-import { IDatabaseService } from '../modules/database/IDatabase.service'
-import { DatabaseService } from '../modules/database/database.service'
+import { IDatabaseService } from '../core/database/IDatabase.service'
+import { DatabaseService } from '../core/database/database.service'
+import { IChatRepository } from '../modules/chat/interfaces/IChat.repository'
+import { ChatRepository } from '../modules/chat/chat.repository'
+import { IChatService } from '../modules/chat/interfaces/IChat.service'
+import { ChatService } from '../modules/chat/chat.service'
+import { ChatResolver } from '../modules/chat/chat.resolver'
 
 
 export const bindings = new ContainerModule(bind => {
@@ -19,4 +24,8 @@ export const bindings = new ContainerModule(bind => {
     bind<IGraphQLSchemaService>(TYPES.IGraphQLSchemaService).to(GraphQLSchemaService)
 
     bind<IDatabaseService>(TYPES.IDatabaseService).to(DatabaseService)
+
+    bind<IChatRepository>(TYPES.IChatRepository).to(ChatRepository)
+    bind<IChatService>(TYPES.IChatService).to(ChatService)
+    bind<ChatResolver>(ChatResolver).toSelf()
 })
--- src/container/types.ts
+++ src/container/types.ts
@@ -1,7 +1,12 @@
+import { IChatService } from '../modules/chat/interfaces/IChat.service'
+
 export const TYPES = {
     IHttpServerService: Symbol.for('IHttpServerService'),
     IWsServerService: Symbol.for('IWsServerService'),
     IApolloServerService: Symbol.for('IApolloServerService'),
     IGraphQLSchemaService: Symbol.for('IGraphQLSchemaService'),
     IDatabaseService: Symbol.for('IDatabaseService'),
+    IChatRepository: Symbol.for('IChatRepository'),
+    IChatService: Symbol.for('IChatService'),
+    ChatResolver: Symbol.for('ChatResolver'),
 }
