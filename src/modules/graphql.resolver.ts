ea5ab992594189721a9e9390ba435364e92e7229
davidcoderistov <davidcoderistov@gmail.com>
2023-07-03 20:55:26 +0200

    Add UserResolver to graphql resolver

--- src/modules/graphql.resolver.ts
+++ src/modules/graphql.resolver.ts
@@ -1,7 +1,9 @@
 import { ChatResolver } from './chat/chat.resolver'
+import { UserResolver } from './user/user.resolver'
 
 const graphqlResolver = [
     ChatResolver,
+    UserResolver,
 ]
 
 export default graphqlResolver
