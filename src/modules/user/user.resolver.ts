694076a8ee28d85941e795ba1e8764d5b05af347
davidcoderistov <davidcoderistov@gmail.com>
2023-07-03 20:54:45 +0200

    Create user resolver & add signUp method

---
+++ src/modules/user/user.resolver.ts
@@ -0,0 +1,21 @@
+import { inject, injectable } from 'inversify'
+import { Resolver, Mutation, Args } from 'type-graphql'
+import { TYPES } from '../../container/types'
+import { IUserService } from './interfaces/IUser.service'
+import { SignUpDto } from './dtos/sign-up.dto'
+import { AuthUserModel } from './graphql.models/auth-user.model'
+
+
+@injectable()
+@Resolver()
+export class UserResolver {
+
+    constructor(
+        @inject(TYPES.IUserService) private readonly _userService: IUserService) {
+    }
+
+    @Mutation(() => AuthUserModel)
+    public async signUp(@Args() signUpDto: SignUpDto): Promise<AuthUserModel> {
+        return this._userService.createUser(signUpDto)
+    }
+}
