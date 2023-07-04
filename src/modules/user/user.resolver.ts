171ed7e9bf875fac373f8217a497c0324e2a6ba6
davidcoderistov <davidcoderistov@gmail.com>
2023-07-04 12:58:40 +0200

    Implement signIn user resolver method

--- src/modules/user/user.resolver.ts
+++ src/modules/user/user.resolver.ts
@@ -3,6 +3,7 @@ import { Resolver, Mutation, Args } from 'type-graphql'
 import { TYPES } from '../../container/types'
 import { IUserService } from './interfaces/IUser.service'
 import { SignUpDto } from './dtos/sign-up.dto'
+import { SignInDto } from './dtos/sign-in.dto'
 import { AuthUserModel } from './graphql.models/auth-user.model'
 
 
@@ -17,5 +18,10 @@ export class UserResolver {
     @Mutation(() => AuthUserModel)
     public async signUp(@Args() signUpDto: SignUpDto): Promise<AuthUserModel> {
         return this._userService.signUp(signUpDto)
+    }
+
+    @Mutation(() => AuthUserModel)
+    public async signIn(@Args() signInDto: SignInDto): Promise<AuthUserModel> {
+        return this._userService.signIn(signInDto)
     }
 }
