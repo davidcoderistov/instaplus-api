79e52e0ced5231289f8d452da3ab1306f091cbda
davidcoderistov <davidcoderistov@gmail.com>
2023-07-04 12:58:09 +0200

    Implement signIn user service method

--- src/modules/user/interfaces/IUser.service.ts
+++ src/modules/user/interfaces/IUser.service.ts
@@ -1,7 +1,10 @@
 import { SignUpDto } from '../dtos/sign-up.dto'
+import { SignInDto } from '../dtos/sign-in.dto'
 import { AuthUserModel } from '../graphql.models/auth-user.model'
 
 
 export interface IUserService {
     signUp(signUpDto: SignUpDto): Promise<AuthUserModel>
+
+    signIn(signInDto: SignInDto): Promise<AuthUserModel>
 }
--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -2,9 +2,11 @@ import { inject, injectable } from 'inversify'
 import { IUserRepository } from './interfaces/IUser.repository'
 import { IUserService } from './interfaces/IUser.service'
 import { SignUpDto } from './dtos/sign-up.dto'
+import { SignInDto } from './dtos/sign-in.dto'
 import { AuthUserModel } from './graphql.models/auth-user.model'
 import { TYPES } from '../../container/types'
 import bcrypt from 'bcrypt'
+import { generateAccessToken, generateRefreshToken } from '../../shared/utils/token'
 import { MongoError } from 'mongodb'
 import { Error } from 'mongoose'
 import { CustomValidationException } from '../../shared/exceptions/custom.validation.exception'
@@ -39,6 +41,35 @@ export class UserService implements IUserService {
                 throw new ValidationException(err)
             }
             throw new MongodbServerException('Could not sign up. Please try again later')
+        }
+    }
+
+    public async signIn(signInDto: SignInDto): Promise<AuthUserModel> {
+        try {
+            const user = await this._userRepository.findUserByUsername(signInDto.username)
+            if (!user) {
+                return Promise.reject(new CustomValidationException('username', `User ${signInDto.username} does not exist`))
+            }
+
+            const passwordMatch = await bcrypt.compare(signInDto.password, user.password)
+            if (!passwordMatch) {
+                return Promise.reject(new CustomValidationException('password', 'Wrong password'))
+            }
+
+            const userId = user._id.toString()
+            const refreshToken = generateRefreshToken(userId)
+            const updatedUser = await this._userRepository.updateUserById(userId, { refreshToken })
+            if (updatedUser) {
+                return {
+                    user: updatedUser,
+                    accessToken: generateAccessToken(userId),
+                    refreshToken: updatedUser.refreshToken,
+                }
+            } else {
+                return Promise.reject(new MongodbServerException(`User ${signInDto.username} does not exist`))
+            }
+        } catch (err) {
+            throw new MongodbServerException('Could not sign in. Please try again later')
         }
     }
 }
