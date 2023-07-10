94186175b5d6bac4d885c5a5b66a82b3008f481f
davidcoderistov <davidcoderistov@gmail.com>
2023-07-10 13:17:23 +0200

    FindChatsDto pagination args

--- src/modules/chat/dtos/find-chats.dto.ts
+++ src/modules/chat/dtos/find-chats.dto.ts
@@ -1,15 +1,7 @@
-import { ArgsType, Field, Int } from 'type-graphql'
-import { Min } from 'class-validator'
+import { ArgsType } from 'type-graphql'
+import { PaginationArgs } from '../../../shared/graphql/pagination-args'
 
 
 @ArgsType()
-export class FindChatsDto {
-
-    @Field(() => Int)
-    @Min(0)
-    offset!: number
-
-    @Field(() => Int)
-    @Min(0)
-    limit!: number
+export class FindChatsDto extends PaginationArgs {
 }
