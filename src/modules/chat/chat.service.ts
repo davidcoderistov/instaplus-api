9511231776f05a2b372e510f39af1b364261b5d2
davidcoderistov <davidcoderistov@gmail.com>
2023-07-10 14:48:47 +0200

    Create createChat chat service method

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -1,16 +1,20 @@
 import { inject, injectable } from 'inversify'
 import { IChatService } from './interfaces/IChat.service'
-import { FindChatsDto, FindMessagesByChatIdDto } from './dtos'
+import { FindChatsDto, FindMessagesByChatIdDto, CreateChatDto } from './dtos'
 import { ChatsModel, MessagesModel } from './graphql.models'
+import { IChat } from './db.models/chat.model'
 import { TYPES } from '../../container/types'
 import { IChatRepository } from './interfaces/IChat.repository'
+import { IUserRepository } from '../user/interfaces/IUser.repository'
+import { CustomValidationException } from '../../shared/exceptions'
 
 
 @injectable()
 export class ChatService implements IChatService {
 
     constructor(
-        @inject(TYPES.IChatRepository) private readonly _chatRepository: IChatRepository) {
+        @inject(TYPES.IChatRepository) private readonly _chatRepository: IChatRepository,
+        @inject(TYPES.IUserRepository) private readonly _userRepository: IUserRepository) {
     }
 
     public async findChatsForUser(userId: string, findChatsDto: FindChatsDto): Promise<ChatsModel> {
@@ -19,5 +23,44 @@ export class ChatService implements IChatService {
 
     public async findMessagesByChatId(findMessagesByChatIdDto: FindMessagesByChatIdDto): Promise<MessagesModel> {
         return this._chatRepository.findMessagesByChatId(findMessagesByChatIdDto)
+    }
+
+    public async createChat(createChatDto: CreateChatDto, creatorId: string): Promise<IChat> {
+        try {
+            const chat = await this._chatRepository.findChatByChatMemberIds(createChatDto.chatMemberIds)
+
+            if (chat) {
+                return chat
+            }
+            
+            const creator = await this._userRepository.findUserById(creatorId)
+            if (!creator) {
+                return Promise.reject(new CustomValidationException('creatorId', `User ${creatorId} does not exist`))
+            }
+
+            const chatMembers = await this._userRepository.findUsersByIds(createChatDto.chatMemberIds)
+            if (chatMembers.length !== createChatDto.chatMemberIds.length) {
+                return Promise.reject(new CustomValidationException('chatMembers', `Chat members should all exist`))
+            }
+
+            return this._chatRepository.createChat(
+                {
+                    _id: creator._id,
+                    firstName: creator.firstName,
+                    lastName: creator.lastName,
+                    username: creator.username,
+                    photoUrl: creator.photoUrl,
+                },
+                chatMembers.map(chatMember => ({
+                    _id: chatMember._id,
+                    firstName: chatMember.firstName,
+                    lastName: chatMember.lastName,
+                    username: chatMember.username,
+                    photoUrl: chatMember.photoUrl,
+                })),
+            )
+        } catch (err) {
+            throw err
+        }
     }
 }
--- src/modules/chat/interfaces/IChat.service.ts
+++ src/modules/chat/interfaces/IChat.service.ts
@@ -1,4 +1,5 @@
-import { FindChatsDto, FindMessagesByChatIdDto } from '../dtos'
+import { FindChatsDto, FindMessagesByChatIdDto, CreateChatDto } from '../dtos'
+import { IChat } from '../db.models/chat.model'
 import { ChatsModel, MessagesModel } from '../graphql.models'
 
 
@@ -6,4 +7,6 @@ export interface IChatService {
     findChatsForUser(userId: string, findChatsDto: FindChatsDto): Promise<ChatsModel>
 
     findMessagesByChatId(findMessagesByChatIdDto: FindMessagesByChatIdDto): Promise<MessagesModel>
+
+    createChat(createChatDto: CreateChatDto, creatorId: string): Promise<IChat>
 }
