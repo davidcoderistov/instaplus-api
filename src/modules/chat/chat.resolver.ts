d70a6e90dc4a7e936fdf4f5268c4d685073831df
davidcoderistov <davidcoderistov@gmail.com>
2023-07-10 13:08:07 +0200

    Implement findMessagesByChatIdDto chat resolver method

--- src/modules/chat/chat.resolver.ts
+++ src/modules/chat/chat.resolver.ts
@@ -3,8 +3,9 @@ import { Resolver, Query, Args, Ctx } from 'type-graphql'
 import { TYPES } from '../../container/types'
 import { IChatService } from './interfaces/IChat.service'
 import { Context } from '../../shared/types'
-import { FindChatsDto } from './dtos/find-chats.dto'
+import { FindChatsDto, FindMessagesByChatIdDto } from './dtos'
 import { ChatsModel } from './graphql.models/chats.model'
+import { MessagesModel } from './graphql.models/messages.model'
 
 
 @injectable()
@@ -18,5 +19,10 @@ export class ChatResolver {
     @Query(() => ChatsModel)
     public async findChatsForUser(@Args() findChatsDto: FindChatsDto, @Ctx() { userId }: Context): Promise<ChatsModel> {
         return this._chatService.findChatsForUser(userId, findChatsDto)
+    }
+
+    @Query(() => MessagesModel)
+    public async findMessagesByChatId(@Args() findMessagesByChatIdDto: FindMessagesByChatIdDto): Promise<MessagesModel> {
+        return this._chatService.findMessagesByChatId(findMessagesByChatIdDto)
     }
 }
