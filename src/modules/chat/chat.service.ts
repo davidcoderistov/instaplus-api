45910a40a47eb5163d5f87b786b132f9ca72f50a
davidcoderistov <davidcoderistov@gmail.com>
2023-07-10 13:06:15 +0200

    Implement findMessagesByChatId chat service method

--- src/modules/chat/chat.service.ts
+++ src/modules/chat/chat.service.ts
@@ -1,7 +1,8 @@
 import { inject, injectable } from 'inversify'
 import { IChatService } from './interfaces/IChat.service'
-import { FindChatsDto } from './dtos/find-chats.dto'
+import { FindChatsDto, FindMessagesByChatIdDto } from './dtos'
 import { ChatsModel } from './graphql.models/chats.model'
+import { MessagesModel } from './graphql.models/messages.model'
 import { TYPES } from '../../container/types'
 import { IChatRepository } from './interfaces/IChat.repository'
 
@@ -15,5 +16,9 @@ export class ChatService implements IChatService {
 
     public async findChatsForUser(userId: string, findChatsDto: FindChatsDto): Promise<ChatsModel> {
         return this._chatRepository.findChatsForUser(userId, findChatsDto)
+    }
+
+    public async findMessagesByChatId(findMessagesByChatIdDto: FindMessagesByChatIdDto): Promise<MessagesModel> {
+        return this._chatRepository.findMessagesByChatId(findMessagesByChatIdDto)
     }
 }
--- src/modules/chat/interfaces/IChat.service.ts
+++ src/modules/chat/interfaces/IChat.service.ts
@@ -1,7 +1,10 @@
-import { FindChatsDto } from '../dtos/find-chats.dto'
+import { FindChatsDto, FindMessagesByChatIdDto } from '../dtos'
 import { ChatsModel } from '../graphql.models/chats.model'
+import { MessagesModel } from '../graphql.models/messages.model'
 
 
 export interface IChatService {
     findChatsForUser(userId: string, findChatsDto: FindChatsDto): Promise<ChatsModel>
+
+    findMessagesByChatId(findMessagesByChatIdDto: FindMessagesByChatIdDto): Promise<MessagesModel>
 }
