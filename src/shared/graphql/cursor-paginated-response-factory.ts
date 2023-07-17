9a8bbac08dd5353e7290eceaed2efb054c365a22
davidcoderistov <davidcoderistov@gmail.com>
2023-07-17 12:03:54 +0200

    Return next cursor in queries

--- src/shared/graphql/cursor-paginated-response-factory.ts
+++ src/shared/graphql/cursor-paginated-response-factory.ts
@@ -1,13 +1,23 @@
 import { ObjectType, Field, ClassType } from 'type-graphql'
 
 
+@ObjectType()
+class NextCursor {
+
+    @Field()
+    _id!: string
+
+    @Field()
+    createdAt!: Date
+}
+
 export function CursorPaginatedResponseFactory<T>(TClass: ClassType<T>) {
 
     @ObjectType()
     class CursorPaginatedResponse {
 
-        @Field(() => Boolean)
-        hasNext!: boolean
+        @Field(() => NextCursor, { nullable: true })
+        nextCursor?: NextCursor
 
         @Field(() => [TClass])
         data!: T[]
