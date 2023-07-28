fe3a037f63c7ac564621b9a7bb0a2595658aa2fc
davidcoderistov <davidcoderistov@gmail.com>
2023-07-28 13:05:39 +0200

    Implement store upload with preview & refactor code

--- src/modules/file/IFile.repository.ts
+++ src/modules/file/IFile.repository.ts
@@ -3,4 +3,6 @@ import { FileUpload } from 'graphql-upload-ts'
 
 export interface IFileRepository {
     storeUpload(upload: Promise<FileUpload>, url: string, dimensions?: { height: number, width: number }): Promise<{ photoUrl: string, photoOrientation: 'landscape' | 'portrait' }>
+
+    storePreviewUpload(upload: Promise<FileUpload>, url: string, dimensions?: { height: number, width: number }): Promise<{ photoUrl: string, photoOrientation: 'landscape' | 'portrait', previewPhotoUrl: string }>
 }
--- src/modules/file/file.repository.ts
+++ src/modules/file/file.repository.ts
@@ -1,13 +1,10 @@
 import { injectable } from 'inversify'
 import { IFileRepository } from './IFile.repository'
 import { FileUpload } from 'graphql-upload-ts'
-import { createWriteStream, unlink } from 'fs'
-import path from 'path'
-import { promisify } from 'util'
+import { ReadStream } from 'fs'
+import { Readable } from 'stream'
 import sharp from 'sharp'
 import { v2 as cloudinary } from 'cloudinary'
-
-const unlinkAsync = promisify(unlink)
 
 @injectable()
 export class FileRepository implements IFileRepository {
@@ -17,27 +14,47 @@ export class FileRepository implements IFileRepository {
         url: string,
         dimensions?: { height: number; width: number },
     ): Promise<{ photoUrl: string, photoOrientation: 'landscape' | 'portrait' }> {
-        const { createReadStream, filename } = await upload
+        const { createReadStream } = await upload
         const stream = createReadStream()
-        const storedFileUrl = path.join(__dirname, '..', path.join('/', `${new Date().getTime()}-${filename}`))
 
-        const chunks = []
+        const buffer = await FileRepository.bufferFromStream(stream)
 
-        // Push each chunk of data into the 'chunks' array
-        for await (const chunk of stream) {
-            chunks.push(Buffer.from(chunk))
+        return this.storeImage(buffer, url, dimensions)
+    }
+
+    public async storePreviewUpload(
+        upload: Promise<FileUpload>,
+        url: string,
+        dimensions?: { height: number; width: number },
+    ): Promise<{ photoUrl: string, photoOrientation: 'landscape' | 'portrait', previewPhotoUrl: string }> {
+        const { createReadStream } = await upload
+        const stream = createReadStream()
+
+        const buffer = await FileRepository.bufferFromStream(stream)
+
+        const { photoUrl, photoOrientation } = await this.storeImage(buffer, url, dimensions)
+
+        const { photoUrl: previewPhotoUrl } = await this.storeImage(buffer, url, { height: 1080, width: 1080 })
+
+        return {
+            photoUrl,
+            photoOrientation,
+            previewPhotoUrl,
         }
+    }
 
-        // Concatenate the chunks into a single buffer
-        const fileBuffer = Buffer.concat(chunks)
-
+    private async storeImage(
+        buffer: Buffer,
+        url: string,
+        dimensions?: { height: number; width: number },
+    ): Promise<{ photoUrl: string, photoOrientation: 'landscape' | 'portrait' }> {
         // Get image dimensions
         let width, height
         if (dimensions) {
             width = dimensions.width
             height = dimensions.height
         } else {
-            const dimensions = await sharp(fileBuffer).metadata()
+            const dimensions = await sharp(buffer).metadata()
             if (dimensions.width && dimensions.height) {
                 if (dimensions.width - 200 > dimensions.height) {
                     width = 600
@@ -55,30 +72,45 @@ export class FileRepository implements IFileRepository {
 
 
         // Image processing using Sharp
-        const resizedImageBuffer = await sharp(fileBuffer)
+        const resizedImageBuffer = await sharp(buffer)
             .resize(width, height)
             .toBuffer()
 
-        await new Promise((resolve, reject) => {
-            const writeStream = createWriteStream(storedFileUrl)
-
-            writeStream.on('finish', resolve)
-            writeStream.on('error', (error) => {
-                unlinkAsync(storedFileUrl).then(() => {
-                    reject(error)
-                })
+        const { secure_url: photoUrl }: { secure_url: string } = await new Promise((resolve, reject) => {
+            const stream = cloudinary.uploader.upload_stream({ folder: url }, (err, res) => {
+                if (res) {
+                    resolve(res)
+                } else {
+                    reject(err)
+                }
             })
-
-            writeStream.write(resizedImageBuffer)
-            writeStream.end()
+            FileRepository.bufferToStream(resizedImageBuffer).pipe(stream)
         })
 
-        const result = await cloudinary.uploader.upload(storedFileUrl, { folder: url })
-        await unlinkAsync(storedFileUrl)
-
         return {
-            photoUrl: result.secure_url,
+            photoUrl,
             photoOrientation,
         }
+    }
+
+    private static async bufferFromStream(stream: ReadStream): Promise<Buffer> {
+        const chunks = []
+
+        // Push each chunk of data into the 'chunks' array
+        for await (const chunk of stream) {
+            chunks.push(Buffer.from(chunk))
+        }
+
+        // Concatenate the chunks into a single buffer
+        return Buffer.concat(chunks)
+    }
+
+    private static bufferToStream(buffer: Buffer) {
+        return new Readable({
+            read() {
+                this.push(buffer)
+                this.push(null)
+            },
+        })
     }
 }
