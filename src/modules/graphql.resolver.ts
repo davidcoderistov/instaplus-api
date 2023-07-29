9f5fa0c8384256ec08729992fe331b098bfe56c8
davidcoderistov <davidcoderistov@gmail.com>
2023-07-29 16:02:04 +0200

    Create notification resolver

--- src/modules/graphql.resolver.ts
+++ src/modules/graphql.resolver.ts
@@ -1,9 +1,11 @@
 import { ChatResolver } from './chat/chat.resolver'
 import { UserResolver } from './user/user.resolver'
+import { NotificationResolver } from './notification/notification.resolver'
 
 const graphqlResolver = [
     ChatResolver,
     UserResolver,
+    NotificationResolver,
 ]
 
 export default graphqlResolver
---
+++ src/modules/notification/notification.resolver.ts
@@ -0,0 +1,37 @@
+import { inject, injectable } from 'inversify'
+import {
+    Args,
+    Ctx,
+    Query,
+    Resolver,
+} from 'type-graphql'
+import { TYPES } from '../../container/types'
+import { INotificationService } from './interfaces/INotification.service'
+import { Context } from '../../shared/types'
+import { FindNotificationsDto } from './dtos'
+import { Notifications } from './graphql.models'
+
+
+@injectable()
+@Resolver()
+export class NotificationResolver {
+
+    constructor(
+        @inject(TYPES.INotificationService) private readonly _notificationService: INotificationService) {
+    }
+
+    @Query(() => Notifications)
+    public async findDailyNotifications(@Args() findNotificationsDto: FindNotificationsDto, @Ctx() { userId }: Context): Promise<Notifications> {
+        return this._notificationService.findDailyNotifications(findNotificationsDto, userId)
+    }
+
+    @Query(() => Notifications)
+    public async findWeeklyNotifications(@Args() findNotificationsDto: FindNotificationsDto, @Ctx() { userId }: Context): Promise<Notifications> {
+        return this._notificationService.findWeeklyNotifications(findNotificationsDto, userId)
+    }
+
+    @Query(() => Notifications)
+    public async findEarlierNotifications(@Args() findNotificationsDto: FindNotificationsDto, @Ctx() { userId }: Context): Promise<Notifications> {
+        return this._notificationService.findEarlierNotifications(findNotificationsDto, userId)
+    }
+}
