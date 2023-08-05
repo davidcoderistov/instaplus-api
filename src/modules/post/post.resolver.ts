7bba707ea24dfc569b1acea86951689a634d66a2
davidcoderistov <davidcoderistov@gmail.com>
2023-08-05 22:45:16 +0200

    Implement createPost post resolver method

--- src/modules/post/post.resolver.ts
+++ src/modules/post/post.resolver.ts
@@ -1,12 +1,17 @@
 import { inject, injectable } from 'inversify'
 import {
     Arg,
+    Args,
+    Ctx,
     Query,
+    Mutation,
     Resolver,
 } from 'type-graphql'
 import { IPostService } from './interfaces/IPost.service'
 import { TYPES } from '../../container/types'
-import { Hashtag } from './graphql.models'
+import { Hashtag, Post } from './graphql.models'
+import { CreatePostDto } from './dtos'
+import { Context } from '../../shared/types'
 
 
 @injectable()
@@ -20,5 +25,10 @@ export class PostResolver {
     @Query(() => [Hashtag])
     public async findHashtagsBySearchQuery(@Arg('searchQuery') searchQuery: string): Promise<Hashtag[]> {
         return await this._postService.findHashtagsBySearchQuery(searchQuery, 15) as unknown as Hashtag[]
+    }
+
+    @Mutation(() => Post)
+    public async createPost(@Args() createPostDto: CreatePostDto, @Ctx() { userId }: Context): Promise<Post> {
+        return await this._postService.createPost(createPostDto, userId) as unknown as Post
     }
 }
