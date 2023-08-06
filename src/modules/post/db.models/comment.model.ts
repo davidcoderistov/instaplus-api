102e9d1a5ebec79d09b8bda1e2b460d44e0a0da2
davidcoderistov <davidcoderistov@gmail.com>
2023-08-06 17:47:50 +0200

    Create Comment mongoose model

---
+++ src/modules/post/db.models/comment.model.ts
@@ -0,0 +1,27 @@
+import { Schema, Types, model, SchemaTimestampsConfig } from 'mongoose'
+
+
+const CommentSchema = new Schema({
+    text: {
+        type: Schema.Types.String,
+        required: [true, 'Comment text is required'],
+    },
+    postId: {
+        type: Schema.Types.String,
+        required: [true, 'Post is required'],
+        ref: 'Post',
+    },
+    userId: {
+        type: Schema.Types.String,
+        required: [true, 'User is required'],
+        ref: 'User',
+    },
+}, { timestamps: true })
+
+export interface IComment extends SchemaTimestampsConfig {
+    _id: Types.ObjectId
+    postId: string
+    userId: string
+}
+
+export default model('Comment', CommentSchema)
