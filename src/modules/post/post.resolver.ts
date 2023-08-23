6e2f7e19f94906a0f11601c9f6bc7c37e945ee95
davidcoderistov <davidcoderistov@gmail.com>
2023-08-23 12:53:39 +0200

    Implement findCommentReplies post resolver method

--- src/modules/post/post.resolver.ts
+++ src/modules/post/post.resolver.ts
@@ -19,6 +19,7 @@ import {
     CommentsForPost,
     UsersWhoLikedComment,
     CommentLike,
+    CommentReplies,
 } from './graphql.models'
 import {
     CreatePostDto,
@@ -26,6 +27,7 @@ import {
     FindUsersWhoLikedPostDto,
     FindCommentsForPostDto,
     FindUsersWhoLikedCommentDto,
+    FindCommentRepliesDto,
 } from './dtos'
 import { Context } from '../../shared/types'
 
@@ -103,5 +105,10 @@ export class PostResolver {
     @Mutation(() => CommentLike)
     public async unlikeComment(@Arg('commentId') commentId: string, @Ctx() { userId }: Context): Promise<CommentLike> {
         return await this._postService.unlikeComment(commentId, userId) as unknown as CommentLike
+    }
+
+    @Query(() => CommentReplies)
+    public async findCommentReplies(@Args() findCommentRepliesDto: FindCommentRepliesDto, @Ctx() { userId }: Context): Promise<CommentReplies> {
+        return this._postService.findCommentReplies(findCommentRepliesDto, userId)
     }
 }
