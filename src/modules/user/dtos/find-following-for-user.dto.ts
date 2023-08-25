290cc6c87338b6ee836a2ba37f896c25388d62f0
davidcoderistov <davidcoderistov@gmail.com>
2023-08-25 15:02:26 +0200

    Create FindFollowingForUserDto

---
+++ src/modules/user/dtos/find-following-for-user.dto.ts
@@ -0,0 +1,10 @@
+import { ArgsType, Field } from 'type-graphql'
+import { CursorPaginationArgs } from '../../../shared/graphql/cursor-pagination-args'
+
+
+@ArgsType()
+export class FindFollowingForUserDto extends CursorPaginationArgs {
+
+    @Field(() => String)
+    userId!: string
+}
